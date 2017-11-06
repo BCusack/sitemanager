65dbdfa459e62843c92f84b00c2732655e922f9b
Brian Cusack <briancusack78@gmail.com>
2017-11-06 17:03:07 +1100

    envir

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyC-kUz571upwuP_BdRAex1FNBgvbZ8aolY",
+    authDomain: "sitedb-8fe75.firebaseapp.com",
+    databaseURL: "https://sitedb-8fe75.firebaseio.com",
+    projectId: "sitedb-8fe75",
+    storageBucket: "sitedb-8fe75.appspot.com",
+    messagingSenderId: "40767364275"
   }
 };
